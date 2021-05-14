0900fc8ad8be32e19b18b76b5e477480462ade8a
xieyezi <1435398529@qq.com>
2021-05-14 10:14:16 +0800

    feat: add types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 // import { create } from './create'
 
-import { GetState, SetState, State, StoreApi } from './type'
+import createImpl, {
+	GetState,
+	SetState,
+	State,
+	StoreApi,
+	StateSelector
+} from './create'
 
 export type StateCreator<T extends State, CustomSetState = SetState<T>> = (
 	set: CustomSetState,
@@ -12,4 +18,11 @@ export function create<TState extends State>(
 	createState: StateCreator<TState> | StoreApi<TState>
 ) {
 	console.log(createState)
+	const storeApi: StoreApi<TState> =
+		typeof createState === 'function' ? createImpl(createState) : createState
+	console.log(storeApi)
+
+	const useStore: any = <StateSlice>(
+		selector: StateSelector<TState, StateSlice> = api.getState as any
+	) => {}
 }
